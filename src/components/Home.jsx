1086cb2ef604b8a1d0ef7c824d3589490704ffeb
raveena <raveenar.kale@gmail.com>
2022-12-19 23:57:23 +0530

    finally done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
-import { Link } from "react-scroll";
+import { Button, Link } from "react-scroll";
 import Typewriter from "typewriter-effect";
 
 const Home = () => {
@@ -9,10 +9,10 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full w-full">
           <h2 className="text-5xl sm:text-5xl font-bold tracking-wide">
-          I am Raveena <span style={{ color: "#937DC2" }}>Kale</span>
+            I am Raveena <span style={{ color: "#937DC2" }}>Kale</span>
           </h2>
           <p className="py-4 max-w-md tracking-wide text-lg ">
-          <span className="text-5xl font-Big"> I am a</span>
+            <span className="text-5xl font-Big"> I am a</span>
             <span className="text-red-600 text-3xl font-Big">
               <Typewriter
                 options={{
@@ -24,20 +24,24 @@ const Home = () => {
                   loop: true,
                 }}
               />
-            </span> Analytical and detail-oriented aspiring Full Stack web Developer. Worked through 1200+ hours of Bootcamp structure.
+            </span>{" "}
+            Analytical and detail-oriented aspiring Full Stack web Developer.
+            Worked through 1200+ hours of Bootcamp structure.
           </p>
           <div>
-            <Link
-              to="Project"
-              smooth
-              duration={500}
-              className="group text-white  w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-cyan-500 cursor-pointer"
-            >
-              Portfolio
+            <button className="group text-white  w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-cyan-500 cursor-pointer">
+              <a
+                href="/Raveena-Kale-Resume.pdf"
+                download={true}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Resume
+              </a>
               <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
-            </Link>
+            </button>
           </div>
         </div>
 
