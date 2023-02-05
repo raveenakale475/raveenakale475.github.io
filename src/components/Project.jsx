e4cb18728fa9843033212b06dc2c7dd6c2bd9b1e
raveena <raveenar.kale@gmail.com>
2023-02-05 23:52:01 +0530

    project changes have been updated

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Lyst from "../assets/portfolio/Lyst.jpg";
-import Hotstar from "../assets/portfolio/hotstar.png"
+import Hotstar from "../assets/portfolio/hotstar.png";
 import Myntra from "../assets/portfolio/Myntra.png";
 import RelianceDigital from "../assets/portfolio/RelianceDigital.png";
+import GlassCart from "../assets/portfolio/GlassCart.png";
 import { FaGithub, FaReact, FaBootstrap } from "react-icons/fa";
 import { SiNetlify } from "react-icons/si";
 import html from "../assets/html.png";
@@ -11,26 +12,33 @@ import javascript from "../assets/javascript.png";
 import reactImage from "../assets/react.png";
 import Bootsrap from "../assets/bootstarp.png";
 import Redux from "../assets/redux.png";
+import Chakraui from "../assets/chakraui.png";
+import Nodejs from "../assets/nodejs.png";
+import MongoDb from "../assets/mongodb.png";
+import ExpressJs from "../assets/express.png";
+import StyledComponent from "../assets/StyledComponent.png";
 
 const Project = () => {
   const portfolios = [
     {
       id: 1,
-      src: Hotstar,
-      github: "https://github.com/raveenakale475/Hotstar",
-      netlify: "https://hotstar-rrk.netlify.app/",
-      websiteName: "Hotstar Clone",
-      des: "Hotstar is a website to watch shows, movies, sports and TV channels for entertainment, & more with The Disney Bundle.",
-      TechStack: [html, css, javascript, Bootsrap],
+      src: RelianceDigital,
+      github: "https://github.com/RajParmar03/Reliance_digital_clone",
+      netlify: "https://delightful-twilight-10e5bd.netlify.app/",
+      websiteName: "RelianceDigital Clone",
+      des: "RelianceDigital is an E-commerce website to buy Electronics Online.",
+      TechStack: [reactImage, Redux, Chakraui, css],
+      Area: "Area of responsibility:- cartpage, successpage, coupon code functionality and Responsiveness",
     },
     {
       id: 2,
-      src: Myntra,
-      github: "https://github.com/sandeshjadhav5/exotic-tail-7975",
-      netlify: "https://myntraclonemasai.netlify.app/",
-      websiteName: "Myntra Clone",
-      des: "Myntra is an E-commerce website popular for clothing and online shopping all materials which needed in day to day life.",
-      TechStack: [html, css, javascript, Bootsrap],
+      src: GlassCart,
+      github: "https://github.com/souravpl8092/Lenskart_Clone",
+      netlify: "https://masai-glasscart.netlify.app/",
+      websiteName: "Lenskart Clone",
+      des: "Lenskart is an e-commerce website that sells eyeglasses, contact lenses, and sunglasses.",
+      TechStack: [reactImage, Redux, Chakraui, css, Nodejs, ExpressJs, MongoDb],
+      Area: "Area of responsibility:- cartpage, coupon code functionality and Responsiveness",
     },
     {
       id: 3,
@@ -39,16 +47,18 @@ const Project = () => {
       netlify: "https://lyst-rrk.netlify.app/",
       websiteName: "Lyst Clone",
       des: "Lyst is an E-commerce website for online shopping.",
-      TechStack: [html, css, javascript, reactImage, Redux],
+      TechStack: [reactImage, Chakraui, StyledComponent],
+      Area: "Area of responsibility:- Individual project",
     },
     {
       id: 4,
-      src: RelianceDigital,
-      github: "https://github.com/RajParmar03/Reliance_digital_clone",
-      netlify: "https://delightful-twilight-10e5bd.netlify.app/",
-      websiteName: "RelianceDigital Clone",
-      des: "RelianceDigital is an E-commerce website to buy Electronics Online.",
-      TechStack: [html, css, javascript, reactImage, Redux],
+      src: Hotstar,
+      github: "https://github.com/raveenakale475/Hotstar",
+      netlify: "https://hotstar-rrk.netlify.app/",
+      websiteName: "Hotstar Clone",
+      des: "Hotstar is a website to watch shows, movies, sports and TV channels for entertainment, & more with The Disney Bundle.",
+      TechStack: [html, css, javascript],
+      Area: "Area of responsibility:- Individual project",
     },
   ];
 
@@ -66,7 +76,16 @@ const Project = () => {
 
         <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-8 px-12 sm:px-0">
           {portfolios.map(
-            ({ id, src, github, netlify, websiteName, des, TechStack }) => (
+            ({
+              id,
+              src,
+              github,
+              netlify,
+              websiteName,
+              des,
+              TechStack,
+              Area,
+            }) => (
               <div
                 key={id}
                 className="shadow-md shadow-[#937DC2] rounded-lg font-Big text-xl px-5 bg-[#937DC2] text-white"
@@ -88,12 +107,14 @@ const Project = () => {
                     />
                   ))}
                 </div>
+                <h2 className="text-lg mt-3 text-center">{Area}</h2>
                 <div className="flex items-center  justify-around">
                   <div>
                     <a
                       className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
                       href={github}
                       target="_blank"
+                      rel="noreferrer"
                     >
                       <FaGithub size={40} />
                     </a>
@@ -103,6 +124,7 @@ const Project = () => {
                       className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
                       href={netlify}
                       target="_blank"
+                      rel="noreferrer"
                     >
                       <SiNetlify size={40} />
                     </a>
